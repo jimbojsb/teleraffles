e4e8b81b371705d236ead0a2b6138073d469459f
Josh Butts <josh@joshbutts.com>
2013-11-14 19:52:25 -0600

    add built in webserver support

--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
 <?php
+if (php_sapi_name() == 'cli-server'
+    && !in_array($_SERVER['REQUEST_URI'], ['/', 'index.php'])
+    && file_exists(__DIR__ . $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
 require_once __DIR__ . '/../src/run.php';
